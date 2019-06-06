8feb8eea6b5ef79ef066d98e86af7cda4a31e258
salimgimatdinov <salimgimatdinov@gmail.com>
2019-06-06 10:35:28 -0500

    lesson 10

---
+++ index.php
@@ -0,0 +1,27 @@
+<?
+
+class Person {
+	public $name;
+	public $speciality;
+	public $age;
+
+	public function __construct($name, $spec, $age){
+		$this->name = $name;
+		$this->speciality = $spec;
+		$this->age = $age;
+	}
+
+	public function greeting(){
+		echo "Hello! My name is " . $this->name  . ". I am " . $this->speciality  ." and " . $this->age  ." years old. ";
+	}
+}
+
+$person1 = new Person('Peter', 'Programmer', 25);
+$person1->greeting();
+
+echo "<br><br>";
+
+$person2 = new Person('Jane', 'Web-designer', 22);
+$person2->greeting();
+
+?>
